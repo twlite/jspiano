4f1225862c485455db9ee6df755d7c4f98499a9c
Snowflake107 <devsnowflake107@gmail.com>
2020-12-31 12:12:00 +0545

    v1.0.2

--- README.md
+++ README.md
@@ -10,8 +10,40 @@ $ npm i jspiano
 # Example
 
 ```js
-const { Piano, Sheets } = require("jspiano");
+const { Piano } = require("jspiano");
+const piano = new Piano("C4"); // load piano with octave C4
+
+piano.press("C#", "C4", 1000); // press C# of octave C4 for 1 second
+```
+
+# Sheets
+## Sheet format
+`mysheet.json`
+
+```json
+[
+    {
+        "note": "C",
+        "octave": "C4",
+        "duration": 2000
+    }
+]
+```
+
+## Load Sheets
+
+```js
+const { Piano } = require("jspiano");
 const piano = new Piano("C4");
 
-piano.playSheet(Sheets.get("happybirthday"));
+piano.playSheet("./mysheet.json");
 ```
+
+# Octaves
+Available from `C0` to `C7`.
+
+# Preview
+[![video](https://i.imgur.com/OCe3b4O.png)](https://i.imgur.com/OCe3b4O.mp4 "Preview - Click to Watch!")
+
+# Join my discord
+[![](https://i.imgur.com/f6hNUfc.png)](https://discord.gg/2SUybzb)
---
+++ typings/index.d.ts
@@ -0,0 +1,82 @@
+declare module "jspiano" {
+
+    export type OctaveKey = "C0" | "C1" | "C2" | "C3" | "C4" | "C5" | "C6" | "C7";
+    export type Note = keyof OctaveJSON;
+    export const version: string;
+
+    export interface SheetsManager {
+        all(): string[];
+        has(name: string): boolean;
+        get(name: string): string | null;
+    }
+
+    export const Sheets: SheetsManager;
+
+    export interface Source {
+        path: string;
+        pressNote(frequency: number, duration: number): boolean;
+    }
+
+    export interface PianoNotes {
+        octave: OctaveKey;
+        notes: Octave;
+    }
+
+    export interface OctaveJSON {
+        "C": number;
+        "C#": number;
+        "D": number;
+        "D#": number;
+        "E": number;
+        "F": number;
+        "F#": number;
+        "G": number;
+        "G#": number;
+        "A": number;
+        "A#": number;
+        "B": number;
+    }
+
+    export interface OctaveArray {
+        note: keyof OctaveJSON;
+        frequency: number;
+    }
+
+    export class Octave {
+        C: number;
+        CS: number;
+        D: number;
+        DS: number;
+        E: number;
+        F: number;
+        FS: number;
+        G: number;
+        GS: number;
+        A: number;
+        AS: number;
+        B: number;
+
+        constructor(node?: number);
+
+        static get octaves(): OctaveKey[];
+        static get notes(): Note[];
+        toArray(): OctaveArray[];
+        toJSON(): OctaveJSON;
+    }
+
+    export class Piano {
+
+        notes: PianoNotes[];
+        constructor(octave?: OctaveKey | OctaveKey[]);
+
+        private _createOctave(octavekey: OctaveKey | OctaveKey[]): void;
+
+        press(note: Note, octave: OctaveKey, duration?: number): boolean;
+
+        playSheet(path: string): void;
+
+    }
+
+    export const source: Source;
+
+}
